8c11e7dbfd7bcd14a0c4d81a531c052f2f0bfca1
Filthy-ice <345134522@qq.com>
2022-08-04 22:46:58 +0800

    08.04 up to date

---
+++ learn.c
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+
+//struct Stu
+//{
+//	char name;
+//	int age;
+//};
+//
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	struct Stu arr[n];//这种创建变长数组的语法，C99增加的，但是VS至今仍未实施。
+//	return 0;
+//}
+
+int main()
+{
+	int* p = (int*)malloc(10 * sizeof(int));
+	if (p == NULL)
+	{
+		printf("%s\n", strerror(errno));//必要的检测p是否是空地址，也就是检测malloc是否开辟成功。
+	}
+	else
+	{
+		int i = 0;
+		for (i = 0; i < 10; i++)
+		{
+			*(p + i) = i;
+			printf("%d ", *(p + i));
+		}
+	}
+	free(p);//释放空间，如果下面很多代码，应该节省可用空间。
+	p = NULL;//消除p的痕迹，防止别人找到之前所使用的堆区内存，更加安全。
+	return 0;
+}
