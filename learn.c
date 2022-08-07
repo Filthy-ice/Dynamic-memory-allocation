def286aa829ee0f17057e6daca3ab8953f1c1358
Filthy-ice <345134522@qq.com>
2022-08-07 23:42:42 +0800

    08.07 up to date

--- learn.c
+++ learn.c
@@ -51,3 +51,69 @@
 //	}
 //	return 0;
 //}
+
+
+//笔试题
+//下面这个程序有什么问题？
+//原代码
+//void Getmemory(char* p)
+//{
+//	p = (char*)malloc(100);//开辟的堆区空间赋给了p，但是p是str的传值，函数结束p开辟的空间被收回。str并没有改变。
+//}//开辟的动态内存尚未释放,并且找不到，造成内存泄漏。
+//
+//void Test(void)
+//{
+//	char* str = NULL;
+//	Getmemory(str);//这个函数传参，传过去的是str的值？
+//	strcpy(str, "hello bit");//str是NULL，字符串穿不过去，所以程序崩溃。
+//	printf(str);
+//}
+//
+//int main()
+//{
+//	Test();
+//	return 0;
+//}
+
+//修改第一种写法
+//void Getmemory(char** p)
+//{
+//	*p = (char*)malloc(100);
+//}
+//void Test(void)
+//{
+//	char* str = NULL;
+//	Getmemory(&str);
+//	strcpy(str, "hello bit");
+//	printf(str);
+//
+//	free(str);
+//	str = NULL;
+//}
+//int main()
+//{
+//	Test();
+//	return 0;
+//}
+
+//修改第二种写法
+char* Getmemory(char* p)
+{
+	p = (char*)malloc(100);
+	return p;
+}
+void Test(void)
+{
+	char* str = NULL;
+	str = Getmemory(str);
+	strcpy(str, "hello bit");
+	printf(str);
+	
+	free(str);
+	str = NULL;
+}
+int main()
+{
+	Test();
+	return 0;
+}
